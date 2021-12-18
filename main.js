1373164629bddf1df92593eceb2c34f4c9b3c211
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2021-12-17 23:56:39 -0600

    Add function for botons next and previous

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const url = 'https://api.github.com/users/yass-brenda/repos?per_page=10&page=1';
-
+const url = 'https://api.github.com/users/yass-brenda/repos?per_page=10';
+let page=1;
 
 fetch(url)
     .then(response => response.json())
     .then(data => recorrer_arreglo(data));
+
 
 
 function recorrer_arreglo(array){
@@ -12,20 +13,28 @@ function recorrer_arreglo(array){
         let div = document.createElement('div')
         div.textContent= array[i].name
         div1.appendChild(div)
-        console.log(array[i].per_page)
     }
 }
 
-function boton_anterior(url){
-    let parametros = url.location.search;
-    console.log(parametros)
-    console.log(url)
+function boton_anterior(){
     console.log("anterior")
+    fetch(url)
+        .then(response =>{
+            page -= 1;
+            console.log(page)
+            console.log(url)
+            return  page <=1 ? console.log(url) : console.log(`${url}&page=${page}`)
+        })
+
 }
-function boton_siguiente(url){
-    let parametros = url.location.search;
-    console.log(parametros)
-    console.log(url)
+
+function boton_siguiente(){
     console.log("siguiente")
+    fetch(url)
+        .then(response =>{
+            page += 1;
+            console.log(page)
+            console.log(url)
+        return console.log(`${url}&page=${page}`)})
 }
 
